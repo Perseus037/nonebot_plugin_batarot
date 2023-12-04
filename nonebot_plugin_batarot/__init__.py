7808ec7ba096452c4645bee97b6292687bde7b0a
黑纸折扇 <143330850+Perseus037@users.noreply.github.com>
2023-12-04 18:33:21 +0800

    Update __init__.py

--- nonebot_plugin_batarot/__init__.py
+++ nonebot_plugin_batarot/__init__.py
@@ -2,13 +2,15 @@ from nonebot import on_command
 from nonebot.plugin import PluginMetadata
 from .commands import handle_tarot, handle_tarot_spread, handle_daily_fortune, handle_tarot_reading
 
+__version__ = "0.1.0"
 __plugin_meta__ = PluginMetadata(
     name = "ba塔罗牌，运势与魔法占卜！",
-    description = "塔罗牌，占卜，运势，与特典：原作者塔罗牌解读 作者：Perseus037 <1209228678@qq.com>",
+    description = "塔罗牌，占卜，运势，与特典：原作者塔罗牌解读",
     usage = "使用命令：ba塔罗牌，ba占卜，ba运势，ba塔罗牌解读",
     type="application",
     homepage="https://github.com/Perseus037/nonebot-plugin-batarot",
     supported_adapters = {"nonebot.adapters.onebot.v11"},
+    extra = {"License":MIT,"Author":Perseus037},
 )
 
 tarot = on_command("batarot", aliases={"ba塔罗牌"})
