5273108e1135168b10b74d50ce5ab0a1e543b50f
黑纸折扇 <143330850+Perseus037@users.noreply.github.com>
2024-10-14 21:22:29 +0800

    Update config.py

--- nonebot_plugin_batarot/config.py
+++ nonebot_plugin_batarot/config.py
@@ -3,8 +3,7 @@ from pydantic import BaseSettings
 class Config(BaseSettings):
     forward_mode: bool = False  # 默认值为 False，如果未在 .env 文件中配置
 
-    model_config = {
-        "env_file": ".env.prod",  
-    }
+    class Config:
+        env_file = ".env.prod" 
 
 config = Config()
